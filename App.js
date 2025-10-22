e291bb38e0c30d6d15d3cdc241e19281b7b4b7db
iresvision <iremsaral3@gmail.com>
2025-10-22 11:40:38 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Form1 from "./components/Form1";
+import Test1 from "./components/Test1";
+
+function App() {
+  return (
+    <div>
+      <h1>LAB Görevi</h1>
+      <Form1 />
+      <hr />
+      <Test1 />
+    </div>
+  );
+}
+
+export default App;
